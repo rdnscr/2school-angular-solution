f0bed7c66c56790410d4334b954b7d0e38e05550
Raphael Ritter <raphael.ritter@2BIT.ch>
2020-08-16 13:29:43 +0200

    added debounce mechanism for filter

--- projects/todo-advanced-pipe/src/app/todo/components/todo-filter.component.ts
+++ projects/todo-advanced-pipe/src/app/todo/components/todo-filter.component.ts
@@ -1,24 +1,31 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { tap, debounceTime } from 'rxjs/operators';
 
 @Component({
-    selector: 'todo-filter',
-    templateUrl: './todo-filter.component.html'
+  selector: 'todo-filter',
+  templateUrl: './todo-filter.component.html',
 })
 export class TodoFilterComponent {
-    public filter: string;
-    @Output()
-    public filterChanged = new EventEmitter<string>();
+  public filter: string;
 
-    constructor() {
+  @Output()
+  public filterChanged = new EventEmitter<string>();
 
-    }
+  private keyPress$ = new Subject<string>();
 
-    public valueChanged(value: string) {
-        this.filter = value;
-        this.filterChanged.emit(value);
-    }
+  constructor() {
+    this.keyPress$.pipe(debounceTime(500)).subscribe((value) => {
+      this.filter = value;
+      this.filterChanged.emit(value);
+    });
+  }
 
-    public resetFilter() {
-        this.valueChanged('');
-    }
+  public valueChanged(value: string) {
+    this.keyPress$.next(value);
+  }
+
+  public resetFilter() {
+    this.valueChanged('');
+  }
 }
