713a615725b12bd73d995ea55c14492474ed972b
Raphael Ritter <raphael.ritter@2BIT.ch>
2022-05-02 17:45:56 +0200

    added error check in console for cypres

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,3 +19,15 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 import 'cypress-xpath'
+
+Cypress.on('window:before:load', (win) => {
+  cy.spy(win.console, 'error');
+  cy.spy(win.console, 'warn');
+});
+
+afterEach(() => {
+  cy.window().then((win) => {
+    expect(win.console.error).to.have.callCount(0);
+    expect(win.console.warn).to.have.callCount(0);
+  });
+});
