f8dbe452c303bf13154a747c319285255f4caa75
rdnscr <raphael.ritter@bluewin.ch>
2024-04-24 09:52:47 +0200

    removed aot project

--- projects/todo-aot/src/app/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './helpers.util';
-export * from './todo.state';
--- projects/todo-aot/src/app/todo/services/aot-error.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class TodoAotService {
-    constructor() {
-        console.log('this will not work with aot compiler!');
-    }
-}
